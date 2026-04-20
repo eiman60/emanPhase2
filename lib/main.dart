4a95fbe1c38760d15b3bb7c1b1379bba287227cd
eiman60 <421202305@qu.edu.sa>
2026-04-20 04:58:14 +0300

    Fix web /ask 404 fallback by retrying backend candidates

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -5,7 +8,31 @@ import 'package:google_fonts/google_fonts.dart';
 import 'pages/nusuk_home_page.dart';
 
 void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    final message = details.exceptionAsString();
+    if (_isKnownWebKeyboardInsetsError(message)) {
+      debugPrint('Ignored Flutter web keyboard insets assertion: $message');
+      return;
+    }
+    FlutterError.presentError(details);
+  };
+
+  PlatformDispatcher.instance.onError = (Object error, StackTrace stack) {
+    final message = error.toString();
+    if (_isKnownWebKeyboardInsetsError(message)) {
+      debugPrint('Ignored Flutter web keyboard insets assertion: $message');
+      return true;
+    }
+    return false;
+  };
+
   runApp(const NusukApp());
+}
+
+bool _isKnownWebKeyboardInsetsError(String message) {
+  return kIsWeb &&
+      message.contains('_viewInsets.isNonNegative') &&
+      message.contains('ViewInsets cannot be negative');
 }
 
 class NusukApp extends StatelessWidget {
