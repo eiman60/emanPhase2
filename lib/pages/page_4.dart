60a27fe1540924cd4927dcb47eb092967329539b
eiman60 <421202305@qu.edu.sa>
2026-04-22 03:38:04 +0300

    Stabilize Page 4 animated background for web

--- lib/pages/page_4.dart
+++ lib/pages/page_4.dart
@@ -1,17 +1,106 @@
 import 'package:flutter/material.dart';
 
-class Page4 extends StatelessWidget {
+class Page4 extends StatefulWidget {
   const Page4({super.key});
 
   @override
+  State<Page4> createState() => _Page4State();
+}
+
+class _Page4State extends State<Page4> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 8),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Page 4',
-        style: TextStyle(
-          fontSize: 32,
-          fontWeight: FontWeight.w700,
-          color: Color(0xFF1F2937),
+    return AnimatedBuilder(
+      animation: CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+      builder: (context, _) {
+        final t = _controller.value;
+        return ClipRect(
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              const ColoredBox(color: Color(0xFFF4F5F7)),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Transform.translate(
+                  offset: Offset(22 + (t * 20), -85 - (t * 28)),
+                  child: const _SoftGlow(
+                    size: 260,
+                    color: Color(0xFFF8D768),
+                    opacity: 0.35,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Transform.translate(
+                  offset: Offset(56 + ((1 - t) * 14), -150 - (t * 16)),
+                  child: const _SoftGlow(
+                    size: 170,
+                    color: Color(0xFFF8D768),
+                    opacity: 0.22,
+                  ),
+                ),
+              ),
+              const Center(
+                child: Text(
+                  'Page 4',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFF1F2937),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _SoftGlow extends StatelessWidget {
+  const _SoftGlow({
+    required this.size,
+    required this.color,
+    required this.opacity,
+  });
+
+  final double size;
+  final Color color;
+  final double opacity;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        gradient: RadialGradient(
+          colors: [
+            color.withOpacity(opacity),
+            color.withOpacity(opacity * 0.45),
+            color.withOpacity(0),
+          ],
+          stops: const [0.0, 0.55, 1.0],
         ),
       ),
     );
